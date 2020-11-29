07b6c3e65f87263d2b00e5a6790e16d68a2d4a13
Alex Wood <alexanderclaywood@msn.com>
2020-11-29 15:29:21 -0800

    I win, project with sub projects...

---
+++ controllers/build.gradle.kts
@@ -0,0 +1,32 @@
+// Need to have Kotlin directive with NO version in sub project
+plugins {
+	kotlin("jvm")
+	kotlin("plugin.spring")
+}
+
+// Group Name And JAR version
+group = "com.caveman.controllers"
+version = "1.0.0-SNAPSHOT"
+
+// Module Dependancies
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+// Module Test Framework???
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+// Below is managed in parent file
+//
+//tasks.withType<KotlinCompile> {
+//	kotlinOptions {
+//		freeCompilerArgs = listOf("-Xjsr305=strict")
+//		jvmTarget = "1.8"
+//	}
+//}
---
+++ controllers/src/main/kotlin/com/caveman/controllers/ControllersApplication.kt
@@ -0,0 +1,11 @@
+package com.caveman.controllers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControllersApplication
+
+fun main(args: Array<String>) {
+	runApplication<ControllersApplication>(*args)
+}
